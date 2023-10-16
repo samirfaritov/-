7dc7396dd296977dba79a6c7369ec60814855106
samirfaritov <samirfaritov6@gmail.com>
2023-10-16 20:15:18 +0500

    Циклы

---
+++ main.js
@@ -0,0 +1,256 @@
+
+// let a = [
+//     {
+//         names: "Samir",
+//         age: 10
+//     },
+//     {
+//         names: "Alex",
+//         age: 5
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 15
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 19
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 28
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 80
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 18
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 4
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 1
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 2
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 3
+
+//     },
+// ]
+
+// let kalon = []
+// let mayda = []
+
+// // for (let i = 0; i < a.length; i++) {
+// //     if (a[i].names === "Samir") {
+// //         b.push(a[i])
+// //     }
+// // }
+
+// // console.log(b);
+
+
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i].age > 18) {
+//         kalon.push(a[i].age)
+//     } else {
+//         mayda.push(a[i].age)
+//     }
+// }
+
+
+// console.log(kalon);
+// console.log(mayda);
+
+
+
+// let a = ['Samir', 'Amir', 'Samir', 'Samir']
+
+// let b = []
+
+// for(let i = 0; i < a.length; i++) {
+//     if (a[i] === 'Samir') {
+//         b.push(a[i])
+//     }
+// }
+
+// console.log(b);
+
+
+// let a = ['Samir', 'Amir', 'Samir', 'Samir', 'Samir']
+
+// let b = [
+//     {
+//         son: 0
+//     }
+// ]
+
+// for(let i = 0; i < a.length; i++) {
+//     if (a[i] === 'Samir') {
+//         b[0].son++
+//     }
+// }
+
+
+// console.log(b[0].son);
+
+// let names = [
+//     {
+//         names: "Samir",
+//         age: 10
+//     },
+//     {
+//         names: "Alex",
+//         age: 5
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 15
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 19
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 28
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 80
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 18
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 4
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 1
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 2
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 3
+
+//     },
+// ]
+
+// let users = [
+//     {
+//         son: 0
+//     }
+// ]
+
+
+// for(let i = 0; i < names.length; i++) {
+//     if (names[i].names === "Samir") {
+//         users[0].son++
+//     }
+// }
+
+// console.log(users[0].son);
+
+
+// let names = [
+//     {
+//         names: "Samir",
+//         age: 10
+//     },
+//     {
+//         names: "Alex",
+//         age: 5
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 15
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 19
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 28
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 80
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 18
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 4
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 1
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 2
+
+//     },
+//     {
+//         names: "Samir",
+//         age: 3
+
+//     },
+// ]
+
+
+// let q = []
+
+
+// for(let i = 0; i < names.length; i++) {
+//     if (names[i].age += 5) {
+//         q.push(names[i].age)
+//     }
+// }
+
+// console.log(q);
+
